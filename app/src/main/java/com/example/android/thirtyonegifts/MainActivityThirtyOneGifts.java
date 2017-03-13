ea58d85eb4fec5ef7ce0a5d6db50e3df94601f57
Paula Woods <Paula Woods>
2017-03-13 14:54:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/thirtyonegifts/MainActivityThirtyOneGifts.java
@@ -0,0 +1,13 @@
+package com.example.android.thirtyonegifts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityThirtyOneGifts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_thirty_one_gifts);
+    }
+}
